42694396689458af0e4efebcdfc07cf16c53ed60
root <root@PC-FIXE-NATHAN.localdomain>
2020-12-06 15:32:12 +0100

    LARABASE COMMIT

--- .deploy/deploy-dev.php
+++ .deploy/deploy-dev.php
@@ -11,21 +11,21 @@ class DeployLaravel extends Deploy
 	 */
 	protected function deploy()
 	{
-		$this->execAndLog('composer dump-autoload');
-		$this->execAndLog('php artisan down --render="errors::503"');
-		$this->execAndLog('composer install');
-		$this->execAndLog('composer dump-autoload');
-		$this->execAndLog('php artisan migrate --force');
-		$this->execAndLog('php artisan storage:link');
-		$this->execAndLog('php artisan assets_media:link');
-		$this->execAndLog('php artisan module:link');
-		$this->execAndLog('php artisan config:clear');
-		// Gérer le cache avec traductions, remplace "php artisan route:cache"
-		$this->execAndLog('php artisan route:trans:clear');
-		$this->execAndLog('php artisan view:clear');
-		$this->execAndLog('php artisan up');
-		$this->execAndLog('npm ci');
-		$this->execAndLog('npm run dev');
+        $this->execAndLog('composer dump-autoload -n');
+        $this->execAndLog('php artisan down --render="errors::503"');
+        $this->execAndLog('composer install -n');
+        $this->execAndLog('composer dump-autoload -n');
+        $this->execAndLog('php artisan migrate --force');
+        $this->execAndLog('php artisan storage:link');
+        $this->execAndLog('php artisan assets_media:link');
+        $this->execAndLog('php artisan module:link');
+        $this->execAndLog('php artisan config:clear');
+        // Gérer le cache avec traductions, remplace "php artisan route:cache"
+        $this->execAndLog('php artisan route:trans:clear');
+        $this->execAndLog('php artisan view:clear');
+        $this->execAndLog('php artisan up');
+        $this->execAndLog('npm ci');
+        $this->execAndLog('npm run dev');
 	}
 }
 
