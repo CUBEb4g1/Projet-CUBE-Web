38b117f8af8d6d59b535c9b6279131d784e5c3d8
smachball21 <69909407+smachball21@users.noreply.github.com>
2020-12-09 14:53:03 +0100

    Feature User -> Enable & Disable user

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasLocalePreferen
 	use Notifiable;
 
 	protected $fillable = [
-		'username', 'email', 'firstname', 'lastname', 'password',
+		'username', 'email', 'firstname', 'lastname', 'password', 'active',
 	];
 
 	protected $hidden = [
