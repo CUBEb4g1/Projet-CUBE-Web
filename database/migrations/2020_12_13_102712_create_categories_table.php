c2cbaea18097bb8ecd0a1bd5d193a0c62f8c9ff0
PhoenixiaProds <phoenixia.staff@gmail.com>
2020-12-13 13:38:37 +0100

    Adding 'category' functionnalities group

--- database/migrations/2020_12_13_102712_create_categories_table.php
+++ database/migrations/2020_12_13_102712_create_categories_table.php
@@ -14,10 +14,9 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('label');
-            $table->unsignedInteger('resource_id')->unique();
-            $table->foreign('resource_id')->references('id')->on('resources');
+            $table->foreignId('resource_id')->constrained('resources');
             $table->timestamps();
         });
     }
