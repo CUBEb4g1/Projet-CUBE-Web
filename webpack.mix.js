2b0eec2070cf9caf13e24b97cfb68ae6a6cdfddd
smachball21 <69909407+smachball21@users.noreply.github.com>
2020-12-14 14:27:51 +0100

    Install TinyMCE -> Language Fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,7 +26,7 @@ mix.sass('resources/assets/sass/back/vendor.scss', 'public/css/back')
 mix.copyDirectory('node_modules/tinymce/icons', 'public/node_modules/tinymce/icons');
 mix.copyDirectory('node_modules/tinymce/plugins', 'public/node_modules/tinymce/plugins');
 mix.copyDirectory('node_modules/tinymce/skins', 'public/node_modules/tinymce/skins');
-mix.copyDirectory('node_modules/tinymce/langs', 'public/node_modules/tinymce/langs');
+mix.copyDirectory('resources/assets/tinymce/langs', 'public/node_modules/tinymce/langs');
 mix.copyDirectory('node_modules/tinymce/themes', 'public/node_modules/tinymce/themes');
 mix.copy('node_modules/tinymce/jquery.tinymce.js', 'public/node_modules/tinymce/jquery.tinymce.js');
 mix.copy('node_modules/tinymce/jquery.tinymce.min.js', 'public/node_modules/tinymce/jquery.tinymce.min.js');
