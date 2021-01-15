78ea417680d520d9346a554645bccdb088906f31
Quentin A <quentin.arrachart@toolib.fr>
2021-01-15 10:44:59 +0100

    add relations

--- routes/web.php
+++ routes/web.php
@@ -105,7 +105,7 @@ Route::prefix(config('admin.backoffice_prefix'))->middleware(['auth', 'verified'
 		// === Relations ===
 		Route::name('back.relation.list')->get('relation', [ManageRelationsController::class, 'index']);
 		Route::name('back.relation.form')->get('relation/form', [ManageRelationsController::class, 'form']);
-		Route::name('back.relation.delete')->get('relation/delete/{?relation}', [ManageRelationsController::class, 'delete'])->where(['relation' => '\d+']);
+		Route::name('back.relation.delete')->get('relation/delete/{relation}', [ManageRelationsController::class, 'delete'])->where(['relation' => '\d+']);
 	});
 
     // === Gestion des Ressources === \\
