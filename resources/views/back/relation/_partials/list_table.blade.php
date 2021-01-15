45795046f1be70d7c0900c51f5bf9a4e13835827
Quentin A <quentin.arrachart@toolib.fr>
2021-01-15 10:38:29 +0100

    add relations

--- resources/views/back/relation/_partials/list_table.blade.php
+++ resources/views/back/relation/_partials/list_table.blade.php
@@ -13,26 +13,15 @@
 		<td>{{$relation->label}}</td>
 		<td class="text-right text-nowrap">
 				{{-- Modifier --}}
-				@can('update', $relation)
+				
 					<a href="{{ route('back.relation.form', ['relation' => $relation->id]) }}" class="btn btn-sm" data-toggle="tooltip" title="{{ __('Edit') }}">
 						<i class="fas fa-pencil"></i>
 					</a>
-				@else
-					<span class="btn btn-sm disabled" data-toggle="tooltip" title="{{ __('Cannot be edited') }}">
-						<i class="fas fa-pencil"></i>
-					</span>
-				@endcan
 				@component('back._cmpts.more_actions')
 					{{-- Supprimer --}}
-					@can('delete', $relation)
 						<a href="{{ route('back.relation.delete', ['relation' => $relation->id]) }}" class="dropdown-item text-danger" data-toggle="tooltip" title="{{ __('Delete') }}">
 							<i class="fa fa-times fa-fw"></i>{{ __('Delete') }}
 						</a>
-					@else
-						<span class="dropdown-item text-muted" data-toggle="tooltip" title="{{ __('Cannot be deleted') }}">
-							<i class="fa fa-times fa-fw"></i> {{ __('Cannot be deleted') }}
-						</span>
-					@endcan
 				@endcomponent
 			</td>
 	</tr>
