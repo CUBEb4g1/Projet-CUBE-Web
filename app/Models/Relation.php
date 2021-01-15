37cf6af84d93ff6b8ed786a1064830674f94d6bf
Quentin A <quentin.arrachart@toolib.fr>
2021-01-15 11:00:32 +0100

    add relations

--- app/Models/Relation.php
+++ app/Models/Relation.php
@@ -13,8 +13,8 @@ class Relation extends Model
         'label',
     ];
 
-    $timestamps=false;
-    
+    public $timestamps=false;
+
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
