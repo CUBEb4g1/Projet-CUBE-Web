fb16846bea63bf1c4967065e70f2b380716d5ed7
PhoenixiaProds <phoenixia.staff@gmail.com>
2021-01-21 22:33:47 +0100

    Fixed banner image size

--- resources/views/front/_layouts/banner.blade.php
+++ resources/views/front/_layouts/banner.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-6 p-0 bg-soft-dark">
             <div class="d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
                 <div class="pt-5 pb-5">
-                    <img src="{{ asset_cache('media/favicons/favicon.png') }}" alt="">
+                    <img src="{{ asset_cache('media/favicons/favicon.png') }}" height=180px width=180px alt="">
                     <h1 class="font-weight-medium">Hello, bienvenue !</h1>
                     <h5 class="mb-4 text-dark">Explore dès maintenant de nouvelles possibilités</h5>
                     <a target="_blank" class="btn btn-md btn-block btn-outline-special btn-lg border-0" href="/">Suivre le guide</a>
